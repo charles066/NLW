3452280257713447f37c77c40e571812b301d666
Charles Brito <74731416+charles066@users.noreply.github.com>
2025-07-12 21:55:39 -0300

    Add files via upload

---
+++ NLW/script.js
@@ -0,0 +1,108 @@
+const apiKeyInput = document.getElementById('apiKey')
+const gameSelect = document.getElementById('gameSelect')
+const questionInput = document.getElementById('questionInput')
+const askButton = document.getElementById('askButton')
+const aiResponse = document.getElementById('aiResponse')
+const form = document.getElementById('form')
+
+const markdownToHtml = (text) => {
+    const converter = new showdown.Converter()
+    return converter.makeHtml(text)
+}
+
+//key api AIzaSyC_HDLaa_jMVQvYwpnFvHqJ3JAVyzFE6FY
+const perguntarAI = async (question, game, apiKey) => {
+    const model = "gemini-2.5-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    const pergunta = `
+      ## Especialidade
+      Você é especialista assistente de meta para o jogo ${game}
+
+      ## Tarefa
+      Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estrategias, build e dicas
+
+      ## Regra 
+      - Se você nâo sabe a resposta, responda com 'Nâo sei' e nâo tente inventar uma resposta.
+      - se a pergunta nâo esta relacionada ao jogo, responda com 'Essa pergunta não esta relacionada ao jogo'
+      - considere a data atual ${new Date().toLocaleDateString()}
+      - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+      - Nunca responda itens que vc não tenha certeza de que existe no patch atual.
+    /**
+      ## Resposta
+        Pergunta do usuário : Melhor build rengar junngle
+        resposta : A build mais atual é: \n\n **Itens:**\n\n coloque os itens aqui.\n\n**Runas**exemplo de runas\n\n
+    */
+        ---
+        Aqui está a pergunta do usuário: ${question}
+        `
+    /**
+    const perguntaValorant = ``
+    const perguntaCSGO = ``
+
+    let pergunta = ''
+
+    if (game == 'valorant') {
+        pergunta = perguntaValorant
+    } elseif (game == 'CSGO')
+    
+    elseif( game== 'LOl')
+    */
+    const contents = [{
+        role: "user",
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    const tools = [{
+        google_search: {}
+    }]
+
+    //chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            contents,
+            tools
+        })
+    })
+
+    const data = await response.json()
+    return data.candidates[0].content.parts[0].text
+}
+
+const enviarFormulario = async (event) => {
+    event.preventDefault()
+    const apiKey = apiKeyInput.value
+    const game = gameSelect.value
+    const question = questionInput.value
+
+    if (apiKey == '' || game == '' || question == '') {
+        alert("Por favor, prencha todos os campos")
+        return
+    }
+
+    askButton.disabled = true
+    askButton.textContent = "perguntando..."
+    askButton.classList.add ('loading')
+
+    try{
+        //perguntar para a IA
+       const text = await perguntarAI (question, game, apiKey)
+       aiResponse.querySelector('.response-content').innerHTML = markdownToHtml(text)
+       aiResponse.classList.remove('hidden')
+
+    } catch(error) {
+        console.log ('Erro: ', error)
+    } finally {
+        askButton.disabled = false
+        askButton.textContent ="Perguntar"
+        askButton.classList.remove('loading')
+    }
+}
+form.addEventListener('submit',enviarFormulario)
+
+//​​ chave do certificado EVOLUÇÃO
